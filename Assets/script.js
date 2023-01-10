8637e1ae1a851f00b7ecc8fd96b23440163d630d
Mizuri <pro.1.h@outlook.com>
2023-01-10 15:57:11 -0700

    fixed word

--- Assets/script.js
+++ Assets/script.js
@@ -229,7 +229,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-	var password = generatePassword();
+	var password = genPw();
 	var passwordText = document.querySelector("#password");
 
 	passwordText.value = password;
